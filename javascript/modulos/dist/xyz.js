4eb9d8058a30dc8ba1d42f82dadeb92e8e5a3094
jluis-ll <jlucaslanda@gmail.com>
2026-05-20 16:55:43 -0600

    produccion js

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(() => { function n(o) { return console.log("y:" + o), "fin y" } function l(o) { return n("x"), console.log("x:" + o), "fin x" } function r(o) { return "fin xx" } function e(o) { return console.log("z:" + o), "fin z" } console.log(l("abc")); console.log(r("def")); console.log(l("abc")); console.log(r("def")); console.log(n("ghi")); console.log(e("jkl")); })();
+//# sourceMappingURL=xyz.js.map
